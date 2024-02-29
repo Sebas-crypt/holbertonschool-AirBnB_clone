f6036e24b4cff50c95e75a559162cc9c1702ae95
Robert <123roabel@gmail.com>
2024-02-28 23:50:06 -0400

    fix base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ import models
 class BaseModel:
     """Custom base for all the classes in the AirBnb console project
 
-    Arttributes:
+    Attributes:
         id(str): handles unique user identity
         created_at: assigns current datetime
         updated_at: updates current datetime
@@ -19,33 +19,30 @@ class BaseModel:
         __str__: prints the class name, id, and creates dictionary
         representations of the input values
         save(self): updates instance attributes with current datetime
-        to_dict(self): returns the dictionary values of the instance obj
+        to_dict(self): returns a dictionary containing all keys/values of __dict__ of the instance
 
     """
 
     def __init__(self, *args, **kwargs):
-        """Public instance attributes initialization
-        after creation
+        """Public instance attributes initialization after creation
 
         Args:
             *args(args): arguments
             **kwargs(dict): attribute values
 
         """
-        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
         if not kwargs:
             self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = self.updated_at = datetime.utcnow()
             models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key in ("updated_at", "created_at"):
-                    self.__dict__[key] = datetime.strptime(value, DATE_TIME_FORMAT)
-                elif key.startswith("id"):
-                    self.__dict__[key] = str(value)
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key == "id":
+                    setattr(self, key, str(value))
                 else:
-                    self.__dict__[key] = value
+                    setattr(self, key, value)
 
     def __str__(self):
         """
@@ -55,22 +52,17 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the public instance attribute:
-        'updated_at' - with the current datetime
+        Updates the public instance attribute 'updated_at' with the current datetime
         """
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
         """
-        Method returns a dictionary containing all
-        keys/values of __dict__ instance
+        Method returns a dictionary containing all keys/values of __dict__ of the instance
         """
-        map_objects = {}
-        for key, value in self.__dict__.items():
-            if key in ("created_at", "updated_at"):
-                map_objects[key] = value.isoformat()
-            else:
-                map_objects[key] = value
-        map_objects["__class__"] = self.__class__.__name__
-        return map_objects
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        return obj_dict
