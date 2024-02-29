c8554dcca9208fd327a211c0692dd7120c6ec82e
Sebas-crypt <usefullseba17@gmail.com>
2024-02-29 01:26:56 -0400

    fixing some trailing ehit spaces in models and tests files

--- models/base_model.py
+++ models/base_model.py
@@ -65,7 +65,7 @@ class BaseModel:
 
     def to_dict(self):
         """
-        Method returns a dictionary containing all 
+        Method returns a dictionary containing all
         keys/values of __dict__ instance
         """
         map_objects = {}
--- tests/README.md
+++ tests/README.md
@@ -1,1 +1,3 @@
-tests models
+Python Unittest Model
+The project utilizes the python unittest model to ensure the fucntionalities of the models.
+The following sample script runs a test model: python -m unittest tests/
